e911209412c113bd24a8dadbd2859afccaf83bf4
yetanothercarbot <55005345+yetanothercarbot@users.noreply.github.com>
2023-08-22 17:08:15 +1000

    Update java step in github action

--- lib/ui.dart
+++ lib/ui.dart
@@ -1,7 +1,6 @@
 import 'package:crashmap_app/api.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_map/flutter_map.dart';
-import 'package:flutter_map_supercluster/flutter_map_supercluster.dart';
 import 'package:latlong2/latlong.dart';
 import 'package:provider/provider.dart';
 import 'main.dart';
